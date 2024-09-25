fc95701b33d60524a1a0eca2c53d991eadcf0e74
leocampiol <leodecandido@hotmail.com>
2024-09-25 14:08:36 -0300

    Direcionando todas as telas

--- app/src/main/java/com/example/calculadora_gasolina/tela_distancia.kt
+++ app/src/main/java/com/example/calculadora_gasolina/tela_distancia.kt
@@ -1,20 +1,41 @@
 package com.example.calculadora_gasolina
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class tela_distancia : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_distancia)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val edtDistancia = findViewById<EditText>(R.id.edtDistancia)
+        val Distancia_Destino = edtDistancia.text
+
+        val Btnproximo3 = findViewById<Button>(R.id.btn_proximo_3)
+
+        Btnproximo3.setOnClickListener {
+            val distanciaStr: String = edtDistancia.text.toString()
+
+            if (distanciaStr == "") {
+                Snackbar.make(
+                    edtDistancia,
+                    "Preencha todos os campos!",
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+            } else {
+                val intent_tela4 = Intent(this, tela_resultado::class.java)
+                startActivity(intent_tela4)
+            }
         }
     }
 }
